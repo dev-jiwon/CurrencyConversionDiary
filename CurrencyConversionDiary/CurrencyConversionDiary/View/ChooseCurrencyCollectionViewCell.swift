be6982983f4877eea99b8231073f4ec0db4f1806
dev-jiwon <devjiwon@gmail.com>
2018-10-01 00:42:48 +0900

    화폐 선택 collection View 에 사용될 cell 디자인

---
+++ CurrencyConversionDiary/CurrencyConversionDiary/View/ChooseCurrencyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseCurrencyCollectionViewCell.swift
+//  CurrencyConversionDiary
+//
+//  Created by Na jiwon on 01/10/2018.
+//  Copyright © 2018 Na jiwon. All rights reserved.
+//
+
+import UIKit
+
+class ChooseCurrencyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
