f0785e0b38951dd89857d914ba2294d377758312
dev-jiwon <devjiwon@gmail.com>
2018-10-02 00:00:03 +0900

    커스텀 셀쪽

--- CurrencyConversionDiary/CurrencyConversionDiary/View/ChooseCurrencyCollectionViewCell.swift
+++ CurrencyConversionDiary/CurrencyConversionDiary/View/ChooseCurrencyCollectionViewCell.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ChooseCurrencyCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var countryTextLabel: UILabel!
+    @IBOutlet weak var currencyTextLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        makeMainViewBorder()
+    }
+    
+    func makeMainViewBorder() {
+        mainView.layer.borderWidth = 0.5
+        mainView.layer.borderColor = UIColor.lightGray.cgColor
+        mainView.layer.cornerRadius = mainView.frame.width/2
+        mainView.layer.masksToBounds = true
     }
 
 }
